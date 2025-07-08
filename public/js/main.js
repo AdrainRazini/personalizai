467eeeb8351551b4ee92a2b86e0d666c00ca24e9
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-07-08 20:29:42 +0000

    versão para 10 tmb

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
+// js/main.js
 import { auth, db, provider } from "./firebase.js";
-import { signInWithPopup } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+import { signInWithPopup } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 auth.languageCode = 'pt-BR';
 
@@ -13,13 +14,13 @@ export async function loginWithGoogle() {
     localStorage.setItem("useremail", user.email);
     localStorage.setItem("userphoto", user.photoURL);
 
-    // Agora salva o documento com ID igual ao UID
-    await setDoc(doc(db, "users", user.uid), {
+    // ⚠️ Agora salva os dados em 'users', mantendo campos como role (se já existir)
+    await setDoc(doc(db, "users", user.email), {
       name: user.displayName,
       email: user.email,
       photo: user.photoURL,
       lastLogin: new Date().toISOString()
-    }, { merge: true }); // <-- mantém o campo role se já existir
+    }, { merge: true }); // <- evita apagar o campo role
 
     window.location.href = "user-dashboard.html";
   } catch (error) {
